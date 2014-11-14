016fa48b43c5719f56860b358d068d6598f8bfb8
iz1kga <iz1kga@gmail.com>
2014-11-14 23:53:24 +0100

    added insert in DB

--- txtstandard.py
+++ txtstandard.py
@@ -3,19 +3,20 @@ from datetime import datetime as DT
 from time import strftime
 import re
 
+station = {'fetch_ok': False, 'datetime': None, 'weather': None, 'temperature': None, 'temperature_max': None, 'temperature_max_time': None, 'temperature_min': None, 'temperature_min_time': None, 'relative_humidity': None, 'relative_humidity_max': None, 'relative_humidity_max_time': None, 'relative_humidity_min': None, 'relative_humidity_min_time': None, 'dewpoint': None, 'dewpoint_max': None, 'dewpoint_max_time': None, 'dewpoint_min': None, 'dewpoint_min_time': None, 'pressure': None, 'pressure_max': None, 'pressure_max_time': None, 'pressure_min': None, 'pressure_min_time': None, 'wind_strength': None, 'wind_dir': None, 'wind_strength_max': None, 'wind_dir_max': None, 'wind_max_time': None, 'rain': None, 'rain_rate': None, 'rain_rate_max': None, 'rain_rate_max_time': None, 'rain_month': None, 'rain_year':None}
 
-def gettxtdata(urlfield, DTParser, TParser):
+
+def gettxtdata(urlfield, DTParser, TParser, LastRead):
 	try:	
 		data = url.urlopen(url=str(urlfield),timeout=10000).read(20000)
 		data = re.sub('<.*?>', "", data)
 		data = re.sub(' ', "", data)
 		data = re.sub('[\t\r\f\v]', "", data)
 		data = data.split("\n")
-		print str(data[1]+" "+ data[2])
-		print DT.now()
+		station['fetch_ok'] = True
 		#datetime
 		station['datetime'] = DT.strptime(str(data[1]+" "+ data[2]), DTParser)
-		if station['datetime'] <= DT.now():
+		if (station['datetime'] <= DT.now()) and (station['datetime'] != LastRead):
 			#temperature
 			m = re.search('\d+(\.?\,?)?\d*', data[4])
 			if m != None:
@@ -70,42 +71,44 @@ def gettxtdata(urlfield, DTParser, TParser):
 			#min realtive_humidity_min_time
 			station['relative_humidity_min_time'] = DT.strptime(str(data[20]), TParser).strftime("%H:%M")
 
-			#dew_point
-			m = re.search('\d+(\.?\,?)?\d*', data[16])
+			#dewpoint
+			m = re.search('\d+(\.?\,?)?\d*', data[22])
 			if m != None:
-				station['dew_point'] =  float(m.group(0))
+				station['dewpoint'] =  float(m.group(0))
 			else :
-				station['dew_point'] = 'NaN'	
+				station['dewpoint'] = 'NaN'	
 
-			#dew_point_max
-			m = re.search('\d+(\.?\,?)?\d*', data[5])
+			print station['dewpoint'] 
+
+			#dewpoint_max
+			m = re.search('\d+(\.?\,?)?\d*', data[23])
 			if m != None:
-				station['dew_point_max'] =  float(m.group(0))
+				station['dewpoint_max'] =  float(m.group(0))
 			else :
-				station['dew_point_max'] = 'NaN'		
+				station['dewpoint_max'] = 'NaN'		
 
-			#dew_point_max_time
-			station['dew_point_max_time'] = DT.strptime(str(data[24]), TParser).strftime("%H:%M")
+			#dewpoint_max_time
+			station['dewpoint_max_time'] = DT.strptime(str(data[24]), TParser).strftime("%H:%M")
 
-			#dew_point_min
-			m = re.search('\d+(\.?\,?)?\d*', data[7])
+			#dewpoint_min
+			m = re.search('\d+(\.?\,?)?\d*', data[25])
 			if m != None:
-				station['dew_point_min'] =  float(m.group(0))
+				station['dewpoint_min'] =  float(m.group(0))
 			else :
-				station['dew_point_min'] = 'NaN'		
+				station['dewpoint_min'] = 'NaN'		
 
-			#dew_point_min_time
-			station['dew_point_min_time'] = DT.strptime(str(data[26]), TParser).strftime("%H:%M")
+			#dewpoint_min_time
+			station['dewpoint_min_time'] = DT.strptime(str(data[26]), TParser).strftime("%H:%M")
 
 			#pressure
-			m = re.search('\d+(\.?\,?)?\d*', data[16])
+			m = re.search('\d+(\.?\,?)?\d*', data[28])
 			if m != None:
 				station['pressure'] =  float(m.group(0))
 			else :
 				station['pressure'] = 'NaN'	
 
 			#pressure_max
-			m = re.search('\d+(\.?\,?)?\d*', data[5])
+			m = re.search('\d+(\.?\,?)?\d*', data[29])
 			if m != None:
 				station['pressure_max'] =  float(m.group(0))
 			else :
@@ -115,7 +118,7 @@ def gettxtdata(urlfield, DTParser, TParser):
 			station['pressure_max_time'] = DT.strptime(str(data[30]), TParser).strftime("%H:%M")
 
 			#pressure_min
-			m = re.search('\d+(\.?\,?)?\d*', data[7])
+			m = re.search('\d+(\.?\,?)?\d*', data[31])
 			if m != None:
 				station['pressure_min'] =  float(m.group(0))
 			else :
@@ -134,9 +137,9 @@ def gettxtdata(urlfield, DTParser, TParser):
 			#wind_direction
 			m = re.search('(\d+)?((N?S?O?W?E?n?s?o?w?e?)+)?', data[35])
 			if m != None:
-				station['wind_direction'] =  m.group(0)
+				station['wind_dir'] =  m.group(0)
 			else :
-				station['wind_direction'] = 'NaN'
+				station['wind_dir'] = 'NaN'
 
 			#wind_strength_max
 			m = re.search('\d+(\.?\,?)?\d*', data[36])
@@ -148,21 +151,69 @@ def gettxtdata(urlfield, DTParser, TParser):
 			#wind_direction_max
 			m = re.search('(\d+)?((N?S?O?W?E?n?s?o?w?e?)+)?', data[35])
 			if m != None:
-				station['wind_direction_max'] =  m.group(0)
+				station['wind_dir_max'] =  m.group(0)
 			else :
-				station['wind_direction_max'] = 'NaN'
+				station['wind_dir_max'] = 'NaN'
 
 			#wind_direction_max_time
-			station['wind_direction_max_time'] = DT.strptime(str(data[38]), TParser).strftime("%H:%M")
-			print "fetched"
+			station['wind_max_time'] = DT.strptime(str(data[38]), TParser).strftime("%H:%M")
+
+			#rain
+			m = re.search('\d+(\.?\,?)?\d*', data[40])
+			if m != None:
+				station['rain'] =  float(m.group(0))
+			else :
+				station['rain'] = 'NaN'	
+
+			#rain_rate
+			m = re.search('\d+(\.?\,?)?\d*', data[41])
+			if m != None:
+				station['rain_rate'] =  float(m.group(0))
+			else :
+				station['rain_rate'] = 'NaN'	
+
+			#rain_rate_max
+			m = re.search('\d+(\.?\,?)?\d*', data[42])
+			if m != None:
+				station['rain_rate_max'] =  float(m.group(0))
+			else :
+				station['rain_rate_max'] = 'NaN'		
+
+			#rain_rate_max_time
+			station['rain_rate_max_time'] = DT.strptime(str(data[43]), TParser).strftime("%H:%M")
+
+			#rain_month
+			m = re.search('\d+(\.?\,?)?\d*', data[44])
+			if m != None:
+				station['rain_month'] =  float(m.group(0))
+			else :
+				station['rain_month'] = 'NaN'	
+
+			#rain_year
+			m = re.search('\d+(\.?\,?)?\d*', data[45])
+			if m != None:
+				station['rain_year'] =  float(m.group(0))
+			else :
+				station['rain_year'] = 'NaN'
 			
-			print "insert done"
+			print "fetched"
+	
+			
+		else:
+			station['fetch_ok'] = False
+			print "not fetched"
+
+		return station
 	except url.HTTPError, e:
 		print "HTTP Error: %r" %e
+		station['fetch_ok'] = False
 	except url.URLError, e:
 		print "URL Error: %r" %e
+		station['fetch_ok'] = False
 	except ValueError, e:
 		print "Parsing Error: %r" %e
+		station['fetch_ok'] = False
 	except Exception, e:
 		print "Generale error: %r" %e
+		station['fetch_ok'] = False
 
